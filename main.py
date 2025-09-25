0824a127ebe92188a192d9a7968359ef24018830
k0278kim <k0278kim@gmail.com>
2025-09-25 09:58:45 +0900

    feat: apikey 추가

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 from fastapi.responses import HTMLResponse
 from datetime import datetime
+from fastapi import Header, HTTPException
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -57,15 +58,18 @@ def get_server_time():
     }
 
 @app.get("/fetch_notion")
-def fetch_notion():
-    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+def fetch_notion(api_key: str = Header(None)):
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    else:
+        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
 
-    res = requests.post(url, headers=headers, json={})
+        res = requests.post(url, headers=headers, json={})
 
-    if res.status_code != 200:
-        return {"error": res.status_code, "message": res.text}
+        if res.status_code != 200:
+            return {"error": res.status_code, "message": res.text}
 
-    return res.json()
+        return res.json()
 
 def block_to_markdown(block, depth=0):
     """한 블록을 Markdown으로 변환"""
@@ -115,7 +119,9 @@ def fetch_block_children(block_id, depth=0):
 
 
 @app.get("/fetch_notion_doc_md")
-def fetch_notion_doc_md(page_id: str):
+def fetch_notion_doc_md(page_id: str, api_key: str = Header(None)):
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
     page_id = page_id.replace("-", "")
     md_blocks = fetch_block_children(page_id)
     return md_blocks
@@ -129,101 +135,113 @@ def response_to_md(res):
     return mds
 
 @app.get("/fetch_notion_snippet")
-def fetch_notion_snippet_ids(date):
-    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+def fetch_notion_snippet_ids(date, api_key: str = Header(None)):
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    else:
+        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
 
-    res = requests.post(url, headers=headers, json={})
-    result = []
+        res = requests.post(url, headers=headers, json={})
+        result = []
 
-    if res.status_code != 200:
-        return {"error": res.status_code, "message": res.text}
-    
-    res = res.json()["results"]
-    
-    for i in res:
-        relations = []
-        for j in i["properties"][DATABASE_AREA]["relation"]:
-            relations.append(j["id"])
-        if (DATABASE_SNIPPET_ID in relations and i["properties"]["날짜"]["date"]):
-            notion_date = i["properties"]["날짜"]["date"]["start"]
-            if (notion_date == date):
-                type = i["properties"][DATABASE_TITLE_ID]["type"]
-                names = []
-                for j in i["properties"][DATABASE_TITLE_ID][type]:
-                    names.append(j["text"]["content"])
-                content = fetch_notion_doc_md(i["id"])
-                result.append({
-                    "id": i["id"],
-                    "name": names,
-                    "relations": relations,
-                    "who": [j["name"] for j in i["properties"]["Who"]["multi_select"]],
-                    "who_email": [USER_EMAIL[j["name"]] for j in i["properties"]["Who"]["multi_select"]],
-                    "content": content
-                })
+        if res.status_code != 200:
+            return {"error": res.status_code, "message": res.text}
+        
+        res = res.json()["results"]
+        
+        for i in res:
+            relations = []
+            for j in i["properties"][DATABASE_AREA]["relation"]:
+                relations.append(j["id"])
+            if (DATABASE_SNIPPET_ID in relations and i["properties"]["날짜"]["date"]):
+                notion_date = i["properties"]["날짜"]["date"]["start"]
+                if (notion_date == date):
+                    type = i["properties"][DATABASE_TITLE_ID]["type"]
+                    names = []
+                    for j in i["properties"][DATABASE_TITLE_ID][type]:
+                        names.append(j["text"]["content"])
+                    content = fetch_notion_doc_md(i["id"])
+                    result.append({
+                        "id": i["id"],
+                        "name": names,
+                        "relations": relations,
+                        "who": [j["name"] for j in i["properties"]["Who"]["multi_select"]],
+                        "who_email": [USER_EMAIL[j["name"]] for j in i["properties"]["Who"]["multi_select"]],
+                        "content": content
+                    })
 
-    return result
+        return result
 
 @app.get("/fetch_notion_snippet_compare_check")
-def fetch_notion_snippet_compare_check(date):
-    result = {
-        "result": []
-    }
-    notion_snippet_ids = fetch_notion_snippet_ids(date)
-    snippets = fetch_snippet(date, date)
+def fetch_notion_snippet_compare_check(date, api_key: str = Header(None)):
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    else:
+        result = {
+            "result": []
+        }
+        notion_snippet_ids = fetch_notion_snippet_ids(date)
+        snippets = fetch_snippet(date, date)
 
-    for notion in notion_snippet_ids:
-        for snippet in snippets:
-            if (notion["who_email"] and notion["who_email"][0] == snippet["user_email"]):
-                print(notion["content"], snippet["content"])
-                if (("\n".join(notion["content"])) == (snippet["content"])):
-                    result["result"].append({ "user_email": notion["who_email"][0], "check": 1 })
-                else:
-                    result["result"].append({ "user_email": notion["who_email"][0], "check": 2 })
+        for notion in notion_snippet_ids:
+            for snippet in snippets:
+                if (notion["who_email"] and notion["who_email"][0] == snippet["user_email"]):
+                    print(notion["content"], snippet["content"])
+                    if (("\n".join(notion["content"])) == (snippet["content"])):
+                        result["result"].append({ "user_email": notion["who_email"][0], "check": 1 })
+                    else:
+                        result["result"].append({ "user_email": notion["who_email"][0], "check": 2 })
 
-    for user_email in USER_EMAIL.values():
-        isin = False
-        for dat in result["result"]:
-            if (dat["user_email"] == user_email):
-                isin = True
-        if (not isin):
-            result["result"].append({ "user_email": user_email, "check": 0 })
+        for user_email in USER_EMAIL.values():
+            isin = False
+            for dat in result["result"]:
+                if (dat["user_email"] == user_email):
+                    isin = True
+            if (not isin):
+                result["result"].append({ "user_email": user_email, "check": 0 })
 
-    return result
+        return result
 
 @app.get("/fetch_notion_page_ids")
-def fetch_notion_page_ids():
-    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+def fetch_notion_page_ids(api_key: str = Header(None)):
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    else:
+        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
 
-    res = requests.post(url, headers=headers, json={})
-    result = []
+        res = requests.post(url, headers=headers, json={})
+        result = []
 
-    if res.status_code != 200:
-        return {"error": res.status_code, "message": res.text}
-    
-    res = res.json()["results"]
-    
-    for i in res:
-        type = i["properties"][DATABASE_TITLE_ID]["type"]
-        result.append({
-            "id": i["id"],
-            "name": [j["text"]["content"] for j in i["properties"][DATABASE_TITLE_ID][type]],
-            "who": [j["name"] for j in i["properties"]["Who"]["multi_select"]]
-        })
+        if res.status_code != 200:
+            return {"error": res.status_code, "message": res.text}
+        
+        res = res.json()["results"]
+        
+        for i in res:
+            type = i["properties"][DATABASE_TITLE_ID]["type"]
+            result.append({
+                "id": i["id"],
+                "name": [j["text"]["content"] for j in i["properties"][DATABASE_TITLE_ID][type]],
+                "who": [j["name"] for j in i["properties"]["Who"]["multi_select"]]
+            })
 
-    return result
+        return result
 
 
 
 @app.get("/fetch_snippet")
-def fetch_snippet(date_from, date_to):
+def fetch_snippet(date_from, date_to, api_key: str = Header(None)):
 
-    url = f"https://n8n.1000.school/webhook/ae38a67a-6dbd-4404-8a54-74c565b1868e?api_id={SNIPPET_TOKEN}&date_from={date_from}&date_to={date_to}"
-    res = requests.get(url)
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    else:
+        url = f"https://n8n.1000.school/webhook/ae38a67a-6dbd-4404-8a54-74c565b1868e?api_id={SNIPPET_TOKEN}&date_from={date_from}&date_to={date_to}"
+        res = requests.get(url)
 
-    if res.status_code != 200:
-        return {"error": res.status_code, "message": res.text}
+        if res.status_code != 200:
+            return {"error": res.status_code, "message": res.text}
 
-    return res.json()[0]["snippets"]
+        return res.json()[0]["snippets"]
 
 from pydantic import BaseModel
 
@@ -233,84 +251,26 @@ class Snippet(BaseModel):
     content: str
 
 @app.post("/add_snippet")
-def add_snippet(snippet: Snippet):
-    url = "https://n8n.1000.school/webhook/0a43fbad-cc6d-4a5f-8727-b387c27de7c8/"
-    data = {
-        "user_email": snippet.user_email,
-        "api_id": SNIPPET_TOKEN,
-        "snippet_date": snippet.snippet_date,
-        "content": snippet.content
-    }
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "text/html"   # ← text/html 응답을 원한다는 힌트
-    }
-    print(data)
-    res = requests.post(url, json=data, headers=headers)
+def add_snippet(snippet: Snippet, api_key: str = Header(None)):
 
-    if res.status_code != 200:
-        return {"error": res.status_code, "message": res.text}
+    if api_key != os.getenv("API_SECRET_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    else:
+        url = "https://n8n.1000.school/webhook/0a43fbad-cc6d-4a5f-8727-b387c27de7c8/"
+        data = {
+            "user_email": snippet.user_email,
+            "api_id": SNIPPET_TOKEN,
+            "snippet_date": snippet.snippet_date,
+            "content": snippet.content
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "text/html"   # ← text/html 응답을 원한다는 힌트
+        }
+        print(data)
+        res = requests.post(url, json=data, headers=headers)
 
-    return res.json()
+        if res.status_code != 200:
+            return {"error": res.status_code, "message": res.text}
 
-@app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <html>
-      <body>
-        <h1>Notion DB 조회</h1>
-        <input type="text" id="textid" />
-        <input type="text" id="snippetid" placeholder="snippet 작성" />
-        <button onclick="fetchPageIds()">페이지 ID 가져오기</button>
-        <button onclick="fetchData()">자세히 조회하기</button>
-        <button onclick="fetchDocData(document.getElementById('textid').value)">md</button>
-        <button onclick="fetchSnippet('2025-09-22', '2025-09-22')">스니펫 불러오기</button>
-        <button onclick="addSnippet('k0278kim@gachon.ac.kr', '2025-09-23', document.getElementById('snippetid').value)">스니펫 작성</button>
-        <div style="display: flex;">
-            <pre id="output" style=""></pre>
-            <pre id="output2" style=""></pre>
-        </div>
-        <script>
-
-          async function fetchPageIds() {
-            const res = await fetch('/fetch_notion_page_ids');
-            const data = await res.json();
-            document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-          }
-
-          async function fetchData() {
-            const res = await fetch('/fetch_notion');
-            const data = await res.json();
-            document.getElementById('output').textContent = JSON.stringify(data, null, 2);
-          }
-
-          async function fetchDocData(id) {
-            const res = await fetch('/fetch_notion_doc_md?page_id='+id);
-            const data = await res.json();
-            document.getElementById('output2').textContent = JSON.stringify(data, null, 2);
-          }
-
-          async function fetchSnippet(date_from, date_to) {
-            const res = await fetch('/fetch_snippet?date_from='+date_from+'&date_to='+date_to);
-            const data = await res.json();
-            console.log(data);
-          }
-          async function addSnippet(email, date, content) {
-            console.log("addSnippet");
-            const data = {
-                user_email: email,
-                snippet_date: date,
-                content: content
-            };
-            const res = await fetch("/add_snippet", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data)
-            });
-            const result = await res.json();
-            console.log("서버 응답:", result);
-            }
-        </script>
-      </body>
-    </html>
-    """
+        return res.json()
